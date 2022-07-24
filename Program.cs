3816496357cd9260cda6aabfb434576734374136
kuznetsovmatweyushka <kuznetsovmatweyushka@yandex.ru>
2022-07-24 16:47:09 +0300

    Create count alg

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5] { "hello", "world", "im", "a", "programmer" };
+int count = 0;
+string[] SelectStringsLessThen3(string[] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        if (array[i].Length <= 3) count++;
+        i++;
+    }
